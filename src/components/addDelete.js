65677ea337f8e6a1bec22b79376e7b57dfb1852f
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-04-24 18:00:24 +0300

    Comment on factory

--- src/components/addDelete.js
+++ src/components/addDelete.js
@@ -20,7 +20,7 @@ export function addProject(name) {
 export function deleteProject(id) {
     state.projects = state.projects.filter(p => p.id !== id); //creates new array without removed
     if (state.currentProjectId === id) { // checks if removed isCurrent 
-        if (state.projects.length > 0) { 
+        if (state.projects.length > 0) {
             state.currentProjectId = state.projects[0].id; // sets Current to first item in array
         } else {
             state.currentProjectId = null;
@@ -29,18 +29,13 @@ export function deleteProject(id) {
 }
 
 
-
-export function createNote (tile) {
+export function createNote(tile) { // factory for notes
     return {
-
     }
 }
 
-
 export function addNote(title) {
-
 }
 
 export function deleteNote(id) {
-    
 }
