c942c4a2b5e3dfa9d33a5f1374f5d43a072bd65b
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-05-06 11:50:58 +0300

    Complete Project handler and render

--- src/main.js
+++ src/main.js
@@ -1,20 +1,22 @@
 import "./style.css";
-import { state } from "./components/state.js";
-import {toggleProjModal, toggleNoteModal, toggleAddNoteBtn} from "./components/ui.js";
+import { setCurrentProjectId, state } from "./components/state.js";
+import {
+    bindProjModal, bindNoteModal, updateNoteBtnState, renderProjects, renderNotes
+} from "./components/ui.js";
 import { addProject, deleteProject, addNote, deleteNote } from "./components/appLogic.js";
 
+function renderApp() {
+    renderProjects(handleProjectClick);
+    renderNotes();
+    updateNoteBtnState();
+}
 
-toggleProjModal();
-toggleNoteModal();
-toggleAddNoteBtn();
+function handleProjectClick(id) {
+    setCurrentProjectId(id);
+    renderApp()
+}
 
-// testing playground
-addProject("Work");
-addProject("Bork");
-toggleAddNoteBtn();
-
-console.log("projects:", state.projects);
-console.log("active:", state.currentProjectId);
-deleteProject(state.projects[0].id);
-console.log("after projects:", state.projects);
-console.log("after active:", state.currentProjectId);
+function handleProjBind (projectName) {
+    addProject(projectName);
+    renderApp();
+}
